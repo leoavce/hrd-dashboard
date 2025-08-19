b54a3ddcbaf37f27eb1a10eaa8914e6758396780
leoavce <hrdeveloper.intern@gmail.com>
2025-08-19 16:53:39 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,14 +4,14 @@ import { initializeAuth, browserSessionPersistence } from "https://www.gstatic.c
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// TODO: 실제 값으로 교체
 const firebaseConfig = {
   apiKey: "AIzaSyAFlG7wqAr--sQPup19ztOS5wZk4Kq_xpE",
   authDomain: "dashboard-7bb43.firebaseapp.com",
   projectId: "dashboard-7bb43",
-  storageBucket: "dashboard-7bb43.firebasestorage.app",
+  storageBucket: "dashboard-7bb43.firebasestorage.app", // 문제 시 'dashboard-7bb43.appspot.com'으로 변경
   appId: "1:271394458680:web:9948d64b646bcefdfb6acd",
 };
+
 export const app = initializeApp(firebaseConfig);
 
 // 로그인 유지: **세션 단위** (탭/창 닫으면 해제)
